40ac36170db0310af169b70e632b0c400f71b130
Jasmine Frantz <hyunfrantz@gmail.com>
2021-12-10 00:49:48 -0500

    refactor day3 pt 1

--- aoc_2021/day3/day3-part-1.rb
+++ aoc_2021/day3/day3-part-1.rb
@@ -1,9 +1,8 @@
-gamma_rate = []
-epsilon_rate = []
+gamma_rate = ''
+epsilon_rate = ''
 read_input = File.readlines("day3-input", chomp: true)
 half_of_input_length = read_input.length/2
 read_input.map(&:chars).transpose.each do |a|
-  a.count("0") >  half_of_input_length ? gamma_rate.push("0") : gamma_rate.push("1")
-  a.count("0") <  half_of_input_length ? epsilon_rate.push("0") : epsilon_rate.push("1")
+  a.count("0") >  half_of_input_length ? gamma_rate << ("0") && epsilon_rate << ("1") : gamma_rate << ("1") && epsilon_rate << ("0")
  end
- gamma_rate.join.to_i(2) * epsilon_rate.join.to_i(2)
+ gamma_rate.to_i(2) * epsilon_rate.to_i(2)
