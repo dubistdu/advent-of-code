f76b3625860ed0cc04702e7312c573ce2b40606b
Jasmine Frantz <hyunfrantz@gmail.com>
2021-12-13 10:04:42 -0500

    day3 pt2

--- aoc_2021/day3/day3-part-2.rb
+++ aoc_2021/day3/day3-part-2.rb
@@ -1,10 +1,25 @@
-# life support rating = oxygen generator rating * CO2 scrubber rating
-gamma_rate = []
-epsilon_rate = []
-read_input = File.readlines("day3-input", chomp: true)
-half_of_input_length = read_input.length/2
-read_input.map(&:chars).transpose.each do |a|
-  a.count("0") >  half_of_input_length ? gamma_rate.push("0") : gamma_rate.push("1")
-  a.count("0") <  half_of_input_length ? epsilon_rate.push("0") : epsilon_rate.push("1")
- end
- gamma_rate.join.to_i(2) * epsilon_rate.join.to_i(2)
+report = File.readlines("day3-input", chomp: true)
+
+  most_common_value_report = report
+  least_common_value_report = report
+
+  index = 0
+  while most_common_value_report.length > 1
+    current_bits = most_common_value_report.map(&:chars).transpose[index]
+    more_common_bit = current_bits.count("1") >= current_bits.count("0") ? "1" : "0"
+    most_common_value_report = most_common_value_report.select {|a| a[index]==(more_common_bit)}
+    index += 1
+  end
+  oxygen_generator_rating = most_common_value_report.first.to_i(2)
+  
+  index = 0
+  while least_common_value_report.length > 1
+    current_bits = least_common_value_report.map(&:chars).transpose[index]
+    least_common_bit = current_bits.count("0") <= current_bits.count("1") ? "0" : "1"
+    least_common_value_report = least_common_value_report.select {|a| a[index]==(least_common_bit)}
+    index += 1
+  end
+
+  co2_scrubber_rating = least_common_value_report.first.to_i(2)
+
+  p life_support_rating = oxygen_generator_rating * co2_scrubber_rating 
